22290a4599d76647b1bbaa547dd1ad39617ea22c
Krushnasri-0204 <krushnasrisimharaju0204@gmail.com>
2025-11-13 15:36:43 +0530

    committing

---
+++ example.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def delete_node(self, key):
+        temp = self.head
+        prev = None
+        if temp and temp.data == key:
+            self.head = temp.next
+            return
+        while temp and temp.data != key:
+            prev = temp
+            temp = temp.next
+        if temp:
+            prev.next = temp.next
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+ll = LinkedList()
+ll.insert_at_beginning(10)
+ll.insert_at_beginning(20)
+ll.insert_at_end(30)
+ll.insert_at_end(40)
+ll.display()
+ll.delete_node(20)
+ll.display()
